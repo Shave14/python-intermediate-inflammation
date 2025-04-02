c74136957376260962615eb63c90d8f9be71da04
Christopher Shave (Medical and Dental Sciences) <c.shave@bham.ac.uk>
2025-04-02 11:46:26 +0100

    include tests for more models

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,32 +7,12 @@ import pytest
 import inflammation.models as im
 #from inflammation.models import daily_mean
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
-    
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(im.daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(im.daily_mean(test_input), test_result)
-
+"""
+# Writing the tests individually
 def test_daily_max_equal():
-    """Test that max function works when inputs are equal."""
+    "Test that max function works when inputs are equal.""
+    "
     
     test_input = np.array([[1,4],
                            [1,4],
@@ -43,7 +23,9 @@ def test_daily_max_equal():
     npt.assert_array_equal(im.daily_max(test_input), test_result)
 
 def test_daily_max_variable():
-    """Test that max function works when inputs are equal."""
+    ""
+    "Test that max function works when inputs are equal."
+    ""
     
     test_input = np.array([[1,6],
                            [2,5],
@@ -55,7 +37,9 @@ def test_daily_max_variable():
     
     
 def test_daily_min_equal():
-    """Test that max function works when inputs are equal."""
+    ""
+    "Test that max function works when inputs are equal."
+    ""
     
     test_input = np.array([[1,4],
                            [1,4],
@@ -67,7 +51,9 @@ def test_daily_min_equal():
     
     
 def test_daily_min_variable():
-    """Test that max function works when inputs are equal."""
+    ""
+    "Test that max function works when inputs are equal."
+    ""
     
     test_input = np.array([[1,6],
                            [2,5],
@@ -79,7 +65,9 @@ def test_daily_min_variable():
     
     
 def test_daily_min_bad_equal():
-    """Test that max function works when inputs are equal."""
+    ""
+    "Test that max function works when inputs are equal."
+    ""
     
     test_input = np.array([[1,4],
                            [1,4],
@@ -92,9 +80,69 @@ def test_daily_min_bad_equal():
 ## Could have tested negative numbers
 
 def test_daily_min_string():
-    """Test for TypeError when passing strings"""
+    ""
+    "Test for TypeError when passing strings"
+    ""
 
     with pytest.raises(TypeError): 
         # with is a context manager, which creates an isolated context. Often 
         # used to read files, because it automatically closes the file once read
         error_expected = im.daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+        
+"""
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(im.daily_mean(np.array(test)), np.array(expected))
+    
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [1, 4], [1, 4], [1, 4] ], [1, 4]),
+        ([ [1, 6], [2, 5], [3, 4] ], [3, 6]),
+        ([ [1, 1], [-1, -1], [1, -1]], [1,1])
+    ])
+def test_daily_max(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(im.daily_max(np.array(test)), np.array(expected))
+    
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [1, 4], [1, 4], [1, 4] ], [1, 4]),
+        ([ [1, 6], [2, 5], [3, 4] ], [1, 4]),
+        ([ [1, 1], [-1, -1], [1, -1]], [-1,-1])
+    ])
+def test_daily_min(test, expected):
+    """Test min function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(im.daily_min(np.array(test)), np.array(expected))
+    
+def test_daily_min_string():
+    ""
+    "Test for TypeError when passing strings"
+    ""
+
+    with pytest.raises(TypeError): 
+        # with is a context manager, which creates an isolated context. Often 
+        # used to read files, because it automatically closes the file once read
+        error_expected = im.daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+        
+
+####example to parameterise functions
+
+@pytest.mark.parametrize(
+    "func, test, expected",
+    [
+     (im.daily_mean, [ [0, 0], [0, 0], [0, 0] ], [0, 0])
+     ])
+def test_daily_stats(func, test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(func(np.array(test)), np.array(expected))
+
